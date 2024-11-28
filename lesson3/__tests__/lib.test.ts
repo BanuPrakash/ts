7a75cc31fe9fd7cfbed3eb22d72813acc13fae52
Banuprakash <banuprakashc@yahoo.co.in>
2024-11-28 12:28:55 +0530

    day 2

--- lesson3/__tests__/lib.test.ts
+++ lesson3/__tests__/lib.test.ts
@@ -4,19 +4,19 @@ import { filter, memo } from "../src/lib";
 describe('lib functionality', () => {
     // test spec
     it('should filter elements based on predicate', () => {
-        const elems = [3,2,1,7,8];
-        const predicate = jest.fn((x:number) => x % 2 === 0);
+        const elems = [3, 2, 1, 7, 8];
+        const predicate = jest.fn((x: number) => x % 2 === 0);
         const result = filter(elems, predicate);
         expect(result).toEqual([2, 8]);
         expect(predicate).toHaveBeenCalledTimes(5);
     })
 
     it("test memoization", () => {
-        const fn = jest.fn( (x: number) => x * 2); // mock
+        const fn = jest.fn((x: number) => x * 2); // mock
         const memoized = memo(fn);
         expect(memoized(2)).toBe(4);
         expect(memoized(2)).toBe(4); // cache
-        expect(memoized).toHaveBeenCalledTimes(1);
+        expect(fn).toHaveBeenCalledTimes(1);
     })
 })
 
